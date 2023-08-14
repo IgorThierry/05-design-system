5e560b4929a68b7209f203f3ead61428f279058e
Igor Thierry <igorthierry15@gmail.com>
2023-08-14 19:47:56 -0300

    Addon de acessibilidade

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -11,6 +11,7 @@ const config = {
     '@storybook/addon-links',
     '@storybook/addon-onboarding',
     '@storybook/addon-interactions',
+    '@storybook/addon-a11y',
   ],
   framework: {
     name: getAbsolutePath('@storybook/react-vite'),
